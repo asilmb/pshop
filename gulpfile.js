b8f422a03fa6c9f567143c594f8a5e5a7bf0ef65
asilb <asilbekmubarak@gmail.com>
2019-12-16 16:18:39 +0600

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var gulp         = require('gulp'),
-    gutil        = require('gulp-util'),
     sass         = require('gulp-sass'),
     browserSync  = require('browser-sync'),
     concat       = require('gulp-concat'),
